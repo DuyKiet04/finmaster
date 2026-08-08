c69e7f928a44fb58b16fe1ec9b658eccdf7e6cf5
DuyKiet04 <nguyenduykiet05072004@gmail.com>
2026-08-08 15:27:23 +0700

    update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'finmaster-v39'; // Tăng version lên v28 để ép đè cái lỗi cũ
+const CACHE_NAME = 'finmaster-v40'; // Tăng version lên v28 để ép đè cái lỗi cũ
 
 // Các file nằm ngay trên máy của ông
 const STATIC_ASSETS = [
